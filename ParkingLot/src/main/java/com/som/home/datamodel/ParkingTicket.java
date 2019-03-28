8c189df072274a22069bfbbe72bc559dd523a9c5
Kherwa <Shomveer.Kherwa@emc.com>
2019-03-28 14:44:48 +0530

    usual getters and setters

--- ParkingLot/src/main/java/com/som/home/datamodel/ParkingTicket.java
+++ ParkingLot/src/main/java/com/som/home/datamodel/ParkingTicket.java
@@ -6,12 +6,60 @@ import com.som.home.Constants.ParkingSpotType;
 import com.som.home.Constants.ParkingTicketStatus;
 
 public class ParkingTicket {
-	
+
 	String id;
 	Date startTime;
 	ParkingSpotType spotType;
 	ParkingTicketStatus status;
 	String vehicleRegistrationNumber;
 	Date endTime;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Date getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(Date startTime) {
+		this.startTime = startTime;
+	}
+
+	public ParkingSpotType getSpotType() {
+		return spotType;
+	}
+
+	public void setSpotType(ParkingSpotType spotType) {
+		this.spotType = spotType;
+	}
+
+	public ParkingTicketStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(ParkingTicketStatus status) {
+		this.status = status;
+	}
+
+	public String getVehicleRegistrationNumber() {
+		return vehicleRegistrationNumber;
+	}
+
+	public void setVehicleRegistrationNumber(String vehicleRegistrationNumber) {
+		this.vehicleRegistrationNumber = vehicleRegistrationNumber;
+	}
+
+	public Date getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(Date endTime) {
+		this.endTime = endTime;
+	}
 
 }
