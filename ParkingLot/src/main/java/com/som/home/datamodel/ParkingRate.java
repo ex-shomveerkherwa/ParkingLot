6427b4e455b80db67969624e6d355e78663d2ac1
Kherwa <Shomveer.Kherwa@emc.com>
2019-03-31 16:47:31 +0530

    charges as per vehicle types

--- ParkingLot/src/main/java/com/som/home/datamodel/ParkingRate.java
+++ ParkingLot/src/main/java/com/som/home/datamodel/ParkingRate.java
@@ -1,5 +1,33 @@
 package com.som.home.datamodel;
 
-public interface ParkingRate {
+import com.som.home.Constants.VehicleType;
 
+public class ParkingRate {
+
+	private int numberOfHours;
+	private double rate;
+	
+	public int getNumberOfHours() {
+		return numberOfHours;
+	}
+	public void setNumberOfHours(int numberOfHours) {
+		this.numberOfHours = numberOfHours;
+	}
+	
+	public void setRate(double rate) {
+		this.rate = rate;
+	}
+	
+	public double getRate(VehicleType type) {
+		if(type.equals(VehicleType.MOTORBIKE))
+			return 10;
+		if(type.equals(VehicleType.CAR))
+			return 20;
+		if(type.equals(VehicleType.VAN) || type.equals(VehicleType.ELECTRIC))
+			return 25;
+		if(type.equals(VehicleType.TRUCK))
+			return 30;
+		
+		return 15;
+	}
 }
